df1bf121ae5a4b41708d0d8efb6b9be03d006693
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-09-28 00:15:43 +0300

    Add sidebar logic

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ const {
     executeSlashCommands,
 } = SillyTavern.getContext();
 import { addJQueryHighlight } from './jquery-highlight.js';
-import { debounce } from '../../../utils.js';
+import { getGroupPastChats } from '../../../group-chats.js';
+import { getPastCharacterChats } from '../../../../script.js';
+import { debounce, timestampToMoment, sortMoments, uuidv4 } from '../../../utils.js';
 
+/** @type {HTMLDivElement} */
 const movingDivs = document.getElementById('movingDivs');
+/** @type {HTMLDivElement} */
 const sheld = document.getElementById('sheld');
+/** @type {HTMLDivElement} */
 const chat = document.getElementById('chat');
+/** @type {HTMLDivElement} */
 const topBar = document.createElement('div');
+/** @type {HTMLSelectElement} */
 const chatName = document.createElement('select');
+/** @type {HTMLInputElement} */
 const searchInput = document.createElement('input');
+/** @type {HTMLTemplateElement} */
 const draggableTemplate = document.getElementById('generic_draggable_template');
 
 const icons = [
@@ -141,7 +150,7 @@ function setChatName(name) {
             }
             else {
                 const characterAvatar = context.characters[context.characterId]?.avatar;
-                list.push(...await getListOfCharacterChats(characterAvatar));
+                list.push(...await getListOfCharacterChats(characterAvatar, true));
             }
 
             if (list.length > 0) {
@@ -156,10 +165,17 @@ function setChatName(name) {
                 });
                 chatName.disabled = false;
             }
+
+            await populateSideBar();
         }, 0);
     }
 }
 
+/**
+ * Get list of chat names for a character.
+ * @param {string} avatar Avatar name of the character
+ * @returns {Promise<string[]>} List of chat names
+ */
 async function getListOfCharacterChats(avatar) {
     try {
         const result = await fetch('/api/characters/chats', {
@@ -178,6 +194,25 @@ async function getListOfCharacterChats(avatar) {
         console.error('Failed to get list of character chats', error);
         return [];
     }
+}
+
+async function getChatFiles() {
+    const context = SillyTavern.getContext();
+    const chatId = getCurrentChatId();
+
+    if (!chatId) {
+        return [];
+    }
+
+    if (context.groupId) {
+        return await getGroupPastChats(context.groupId);
+    }
+
+    if (context.characterId !== undefined) {
+        return await getPastCharacterChats(context.characterId);
+    }
+
+    return [];
 }
 
 /**
@@ -246,31 +281,159 @@ function addIcons() {
 function addSideBar() {
     if (!draggableTemplate) {
         console.warn('Draggable template not found. Side bar will not be added.');
+        return;
     }
 
     /** @type {DocumentFragment} */
     const fragment = draggableTemplate.content.cloneNode(true);
     const draggable = fragment.querySelector('.draggable');
     const closeButton = fragment.querySelector('.dragClose');
+
+    if (!draggable || !closeButton) {
+        console.warn('Failed to find draggable or close button. Side bar will not be added.');
+        return;
+    }
+
     draggable.id = 'extensionSideBar';
     closeButton.addEventListener('click', onToggleSidebarClick);
 
     const scrollContainer = document.createElement('div');
-    scrollContainer.classList.add('scrollY');
     scrollContainer.id = 'extensionSideBarContainer';
     draggable.appendChild(scrollContainer);
+
+    const loaderContainer = document.createElement('div');
+    loaderContainer.id = 'extensionSideBarLoader';
+    draggable.appendChild(loaderContainer);
+
+    const loaderIcon = document.createElement('i');
+    loaderIcon.className = 'fa-2x fa-solid fa-gear fa-spin';
+    loaderContainer.appendChild(loaderIcon);
 
     movingDivs.appendChild(draggable);
 }
 
-function onToggleSidebarClick() {
-    document.getElementById('extensionSideBar')?.classList.toggle('visible');
-    document.getElementById('extensionTopBarToggleSidebar')?.classList.toggle('active');
+async function onToggleSidebarClick() {
+    const sidebar = document.getElementById('extensionSideBar');
+    const toggle = document.getElementById('extensionTopBarToggleSidebar');
+    sidebar?.classList.toggle('visible');
+    toggle?.classList.toggle('active');
+
+    if (sidebar?.classList.contains('visible')) {
+        await populateSideBar();
+    }
 }
 
-async function onChatNameChange() {
+async function populateSideBar() {
+    const sidebar = document.getElementById('extensionSideBar');
+    const loader = document.getElementById('extensionSideBarLoader');
+    const container = document.getElementById('extensionSideBarContainer');
+
+    if (!loader || !container || !sidebar) {
+        return;
+    }
+
+    if (!sidebar.classList.contains('visible')) {
+        container.innerHTML = '';
+        return;
+    }
+
+    loader.classList.add('displayNone');
+    const processId = uuidv4();
+    const scrollTop = container.scrollTop;
+    const prettify = x => {
+        x.last_mes = timestampToMoment(x.last_mes);
+        x.file_name = String(x.file_name).replace('.jsonl', '');
+        return x;
+    };
+    container.dataset.processId = processId;
+    const chatId = getCurrentChatId();
+    const chats = (await getChatFiles()).map(prettify).sort((a, b) => sortMoments(a.last_mes, b.last_mes));
+
+    if (container.dataset.processId !== processId) {
+        console.log('Aborting populateSideBar due to process id mismatch');
+        return;
+    }
+
+    container.innerHTML = '';
+
+    for (const chat of chats) {
+        const sideBarItem = document.createElement('div');
+        sideBarItem.classList.add('sideBarItem');
+
+        sideBarItem.addEventListener('click', async () => {
+            if (chat.file_name === chatId || sideBarItem.classList.contains('selected')) {
+                return;
+            }
+
+            container.childNodes.forEach(x => x.classList.remove('selected'));
+            sideBarItem.classList.add('selected');
+            await openChatById(chat.file_name);
+        });
+
+        const isSelected = chat.file_name === chatId;
+        sideBarItem.classList.toggle('selected', isSelected);
+
+        const chatName = document.createElement('div');
+        chatName.classList.add('chatName');
+        chatName.textContent = chat.file_name;
+        chatName.title = chat.file_name;
+
+        const chatDate = document.createElement('small');
+        chatDate.classList.add('chatDate');
+        chatDate.textContent = chat.last_mes.format('l');
+        chatDate.title = chat.last_mes.format('LL LT');
+
+        const chatNameContainer = document.createElement('div');
+        chatNameContainer.classList.add('chatNameContainer');
+        chatNameContainer.append(chatName, chatDate);
+
+        const chatMessage = document.createElement('div');
+        chatMessage.classList.add('chatMessage');
+        chatMessage.textContent = chat.mes;
+        chatMessage.title = chat.mes;
+
+        const chatStats = document.createElement('div');
+        chatStats.classList.add('chatStats');
+
+        const counterBlock = document.createElement('div');
+        counterBlock.classList.add('counterBlock');
+
+        const counterIcon = document.createElement('i');
+        counterIcon.classList.add('fa-solid', 'fa-comment', 'fa-xs');
+
+        const counterText = document.createElement('small');
+        counterText.textContent = chat.chat_items;
+
+        counterBlock.append(counterIcon, counterText);
+
+        const fileSizeText = document.createElement('small');
+        fileSizeText.classList.add('fileSize');
+        fileSizeText.textContent = chat.file_size;
+
+        chatStats.append(counterBlock, fileSizeText);
+
+        const chatMessageContainer = document.createElement('div');
+        chatMessageContainer.classList.add('chatMessageContainer');
+        chatMessageContainer.append(chatMessage, chatStats);
+
+        sideBarItem.append(chatNameContainer, chatMessageContainer);
+        container.appendChild(sideBarItem);
+    }
+
+    container.scrollTop = scrollTop;
+
+    /** @type {HTMLElement} */
+    const selectedElement = container.querySelector('.selected');
+    const isSelectedElementVisible = selectedElement && selectedElement.offsetTop >= container.scrollTop && selectedElement.offsetTop <= container.scrollTop + container.clientHeight;
+    if (!isSelectedElementVisible) {
+        container.scrollTop = selectedElement.offsetTop - container.clientHeight / 2;
+    }
+
+    loader.classList.add('displayNone');
+}
+
+async function openChatById(chatId) {
     const context = SillyTavern.getContext();
-    const chatId = chatName.value;
 
     if (!chatId) {
         return;
@@ -285,6 +448,11 @@ async function onChatNameChange() {
         await openCharacterChat(chatId);
         return;
     }
+}
+
+async function onChatNameChange() {
+    const chatId = chatName.value;
+    await openChatById(chatId);
 }
 
 // Init extension on load
